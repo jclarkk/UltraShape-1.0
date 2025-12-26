cc9b08d7b4351211cd571054ce600584e481e4ff
Jonathan Clark <jclarkk@gmail.com>
2025-12-26 14:44:12 +0200

    Change gradio address

--- scripts/gradio_app.py
+++ scripts/gradio_app.py
@@ -193,4 +193,4 @@ if __name__ == "__main__":
             outputs=[output_model]
         )
         
-    demo.launch(share=args.shar, server_name='0.0.0.0', server_port=7860)
+    demo.launch(share=args.share, server_name='0.0.0.0', server_port=7860)
